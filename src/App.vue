8cbe9190ef52c778602f1eb74aebe733a1de3025
MrJ1M1 <razzaqovjamshidbek1@gmail.com>
2022-10-21 12:41:17 +0500

    this project is all over

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp < 16 ? 'cold' : '' ">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather"/>
+        <input type="text" class="search-bar" placeholder="Enter your favorite country or city ..." v-model="query" @keypress="fetchWeather"/>
       </div>
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
